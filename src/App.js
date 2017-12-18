bf9146f3c1f28afe52ee4b222931fa1d5c5d13e8
mcarson01 <matthcarson01@gmail.com>
2017-12-17 20:32:05 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test:[]
+    };
+  }
+
+  componentDidMount(){
+        axios.get(`http://localhost:3000/api/recap/austin`)
+            .then(response => {this.setState({ test: response.data });})
+            .catch(console.log);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
