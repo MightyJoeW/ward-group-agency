fd191f7379206caff3afa52827dc74c909296c96
mcarson01 <matthcarson01@gmail.com>
2017-12-17 22:38:39 -0600

    Set Up Material-UI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 import router from "../../router";
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return(
-    <div className="App">{router}</div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        {router}
+      </MuiThemeProvider>
     );
   }
 }
