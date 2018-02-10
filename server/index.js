c211cd000cc028dc6d178e20e25a7749b3f3c5dc
mcarson01 <matthcarson01@gmail.com>
2018-02-09 20:17:10 -0600

    removed google sheet api cleaned up the front end

--- server/index.js
+++ server/index.js
@@ -3,45 +3,10 @@ const express = require("express"),
   cors = require("cors"),
   axios = require('axios');
 
-const { 
-    austinRecap,
-    dfwRecap,
-    denverRecap,
-    houstonRecap,
-    kcRecap,
-    lvRecap,
-    losReblesRecap,
-    nashRecap,
-    northernVirginiaRecap,
-    stLucieRecap,
-    sanAntonioRecap,
-    orlandoRecap,
-    sanJoseRecap,
-    drucRecap,
-    mccucRecap
-    } = require("./controller/sheetController");
-
-
 const app = express();
 app.use(json());
 app.use(cors());
 
-//ReST Endpoints Accessing Contollers
-app.get("/api/recap/austin",austinRecap);
-app.get("/api/recap/dfw", dfwRecap);
-app.get("/api/recap/denver", denverRecap);
-app.get("/api/recap/houston", houstonRecap);
-app.get("/api/recap/kc", kcRecap);
-app.get("/api/recap/lv", lvRecap);
-app.get("/api/recap/los_robles", losReblesRecap);
-app.get("/api/recap/nash", nashRecap);
-app.get("/api/recap/north_virginia", northernVirginiaRecap);
-app.get("/api/recap/st_lucie", stLucieRecap);
-app.get("/api/recap/san_antonio", sanAntonioRecap);
-app.get("/api/recap/orlando", orlandoRecap);
-app.get("/api/recap/san_jose", sanJoseRecap);
-app.get("/api/recap/druc", drucRecap);
-app.get("/api/recap/mccuc", mccucRecap);
 
 const port = 3000;
 app.listen(port,()=>{console.log(`Listening On Port ${port}`)});
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./components/Home/Home";
+import Home from "./Views/Home/Home";
 
 export default (
   <Switch>
